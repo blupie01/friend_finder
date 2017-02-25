7ec869c66803e20a27cd4449275eb5c6a11e9942
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-25 00:30:24 -0500

    server.js set up

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+// Set up Express App
+var app = express();
+var PORT = 3000;
+
+// Set up the Express App to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Get routes
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+// Start server to begin listening
+app.listen(PORT, function() {
+	console.log("app listening on PORT " + PORT);
+});
