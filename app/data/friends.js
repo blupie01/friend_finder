2844364beb3a2db4ea0bb1b4cdd9c32bad8ed4b0
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-24 20:36:03 -0500

    updated friend.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,80 @@
 // This file will hold all information from people who fill out the survey.
 // Each new person will be an object that will be pushed into the array.
-var friendsArray = []
+var friendsArray = [
+	{
+		"name": "Aang",
+		"photo": "../public/assets/images/aang.jpg",
+		"scores": [
+			1,
+			3,
+			5,
+			1,
+			4,
+			1,
+			3,
+			2,
+			1,
+			1
+		]
+	}, {
+		"name": "Anri",
+		"photo": "../public/assets/images/anri.jpg",
+		"scores": [
+			3,
+			5,
+			3,
+			3,
+			1,
+			3,
+			5,
+			3,
+			3,
+			3
+		]
+	}, {
+		"name": "Gandalf",
+		"photo": "../public/assets/images/gandalf.jpg",
+		"scores": [
+			5,
+			3,
+			5,
+			5,
+			5,
+			3,
+			5,
+			2,
+			1,
+			3
+		]
+	}, {
+		"name": "Guts",
+		"photo": "../public/assets/images/guts.jpeg",
+		"scores": [
+			5,
+			5,
+			3,
+			1,
+			1,
+			2,
+			3,
+			5,
+			5,
+			5
+		]
+	}, {
+		"name": "Lady Maria",
+		"photo": "../public/assets/images/lady_maria.png",
+		"scores": [
+			3,
+			3,
+			3,
+			1,
+			1,
+			1,
+			5,
+			3,
+			4,
+			3
+		]		
+	}
+];
