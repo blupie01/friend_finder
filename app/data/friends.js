724ac0a9c0c3c9b2d76f84e9555fccf4f69df34e
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-26 00:57:59 -0500

    updated form, added modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,78 +4,23 @@ var friends = [
 	{
 		"name": "Aang",
 		"photo": "../public/assets/images/aang.jpg",
-		"scores": [
-			1,
-			3,
-			5,
-			1,
-			4,
-			1,
-			3,
-			2,
-			1,
-			1
-		]
+		"totalScore": 20
 	}, {
 		"name": "Anri",
 		"photo": "../public/assets/images/anri.jpg",
-		"scores": [
-			3,
-			5,
-			3,
-			3,
-			1,
-			3,
-			5,
-			3,
-			3,
-			3
-		]
+		"totalScore": 35
 	}, {
 		"name": "Gandalf",
 		"photo": "../public/assets/images/gandalf.jpg",
-		"scores": [
-			5,
-			3,
-			5,
-			5,
-			5,
-			3,
-			5,
-			2,
-			1,
-			3
-		]
+		"totalScore": 5
 	}, {
 		"name": "Guts",
 		"photo": "../public/assets/images/guts.jpeg",
-		"scores": [
-			5,
-			5,
-			3,
-			1,
-			1,
-			2,
-			3,
-			5,
-			5,
-			5
-		]
+		"totalScore": 45
 	}, {
 		"name": "Lady Maria",
 		"photo": "../public/assets/images/lady_maria.png",
-		"scores": [
-			3,
-			3,
-			3,
-			1,
-			1,
-			1,
-			5,
-			3,
-			4,
-			3
-		]		
+		"totalScore": 15
 	}
 ];
 
