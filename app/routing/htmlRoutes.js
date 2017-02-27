9710f7cbf52d3ea99647d9d9473c2c6d7e68e363
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-26 20:37:51 -0500

    modified pathing in htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,15 +4,15 @@ var path = require("path");
 module.exports = function(app) {
 	// HTML GET requests
 	app.get("/home", function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/home.html"));
+		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 
 	app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/survey.html"));
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
 
 	// If invalid route redirect to home
 	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/home.html"));
+		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 };
