4e48c502ffbf4b5e9fce4bf7caab415afc9c2eb9
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-26 20:23:31 -0500

    friends.js updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,22 +10,22 @@ var friends = [
 		"name": "Anri",
 		"photo": "../public/assets/images/anri.jpg",
 		"totalScore": 35,
-		"grade": ""
+		"grade": "c"
 	}, {
 		"name": "Gandalf",
 		"photo": "../public/assets/images/gandalf.jpg",
 		"totalScore": 10,
-		"grade": ""
+		"grade": "a"
 	}, {
 		"name": "Guts",
 		"photo": "../public/assets/images/guts.jpeg",
 		"totalScore": 45,
-		"grade": ""
+		"grade": "d"
 	}, {
 		"name": "Lady Maria",
 		"photo": "../public/assets/images/lady_maria.png",
-		"totalScore": 15,
-		"grade": ""
+		"totalScore": 25,
+		"grade": "b"
 	}
 ];
 
